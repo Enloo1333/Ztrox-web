158ee41eb1240b32e589f16f259cacefd5064bb1
VENOM <merantiondiscordbot@gmail.com>
2024-10-27 01:13:50 +0300

    _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Herkese açık Yönetim panel bir bot arıyorsun? Ztrox tam sana göre 82 [/] slash komut sahip Ztrox kullan!"
           />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@MusicMaker" />
@@ -26,7 +26,7 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Herkese açık Yönetim panel bir bot arıyorsun? Ztrox tam sana göre 82 [/] slash komut sahip Ztrox kullan!"
           />
           <meta property="og:image" content="/img/logo.jpg" />
           <meta property="og:image:alt" content="MusicMaker" />
