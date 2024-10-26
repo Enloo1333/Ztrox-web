bb22be442f303e3614b7bb1b0b2e988d64dd9a85
VENOM <merantiondiscordbot@gmail.com>
2024-10-27 01:09:00 +0300

    _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -44,14 +44,14 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Add Bot",
       icon: "fal fa-robot",
       activeIcon: "fab fa-robot",
-      href: "https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=bot%20applications.commands",
+      href: "https://discord.com/api/oauth2/authorize?client_id=1294795967387926548&permissions=277028620608&scope=bot%20applications.commands",
     },
        {
       link: true,
       name: "Vote",
       icon: "fa fa-plus",
       activeIcon: "fa fa-plus",
-      href: "https://top.gg/bot/774043716797071371/vote",
+      href: "https://top.gg/bot/1294795967387926548/vote",
     },
     {
       link: true,
@@ -71,7 +71,7 @@ export default function AwardApp({ Component, pageProps }) {
       />
       <Head>
         <title>
-          MusicMaker
+          Ztrox
         </title>
       </Head>
       <main className="transition-all duration-200 z-10 absolute inset-0 px-5 h-screen max-w-7xl w-full mx-auto">
